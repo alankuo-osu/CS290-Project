478cd9a5c37c3aeff8015b0f6d574dd62d0e5eaf
alankuo-osu <kuoala@oregonstate.edu>
2020-08-01 18:56:50 -0400

    Revert "Update script.js"
    This reverts commit 04a3d88282dec81b5b0644f1f8feb75b4b07d829.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 // Checking if currently on home page
 // Set current slide, start automatic switching
-if (window.location == "https://alankuo-osu.github.io/CS290-Project/index.html" or window.location =="https://alankuo-osu.github.io/CS290-Project/") {
+if (window.location == "https://alankuo-osu.github.io/CS290-Project/index.html") {
 	var curSlide = 1;
 	var interval_id = setInterval(function(){changeSlide(1,0);}, 3000);
 }
@@ -41,7 +41,7 @@ function changeSlide(delta, clicked) {
 // Function for underlining currently active page in nav bar
 function navUnderline() {
 	var currentUrl = window.location;
-	var navLinks = document/.getElementById('navbar').getElementsByTagName('a');
+	var navLinks = document.getElementById('navbar').getElementsByTagName('a');
 	for (let link of navLinks) {
 		if (currentUrl == link.getAttribute('href')) {
 			link.style.textDecoration = "underline"
